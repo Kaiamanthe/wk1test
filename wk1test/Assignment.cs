6d95389018bbf6f729c04b15dc42b6f4bd17a2f4
Khai Ha <Khaiha265@gmail.com>
2025-04-21 09:50:48 -0700

    Add project files.

---
+++ wk1test/Assignment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wk1test
+{
+    public class Assignment
+    {
+        public string Title { get; set; } = "New Assignment";
+    }
+}
---
+++ wk1test/AssignmentTest.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace wk1test
+{
+    public class AssignmentTests
+    {
+        [Fact]
+        public void Assignment_Should_Have_A_Title()
+        {
+            var assignment = new Assignment();
+
+            Assert.NotNull(assignment.Title);
+        }
+    }
+}
